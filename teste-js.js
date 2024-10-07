94826f280c18940169f5d5a14d5938ce651bc8de
miguelzackk <mzsilvaw@gmail.com>
2024-10-07 18:57:26 -0300

    testes-js

---
+++ teste-js.js
@@ -0,0 +1,8 @@
+const fullName = 'Miguel Zacharias da Silva';
+
+const stringToArray = fullName.split(' ');
+
+const username = 'Ola, ' + stringToArray[0] + ' ' + stringToArray[stringToArray.length - 1];
+
+console.log(username);
+
